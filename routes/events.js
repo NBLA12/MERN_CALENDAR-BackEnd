0439cfe93265c09953bbaa5e14cdc69934f5669d
Nadia <nbla.escom@gmail.com>
2023-07-01 07:39:22 -0600

    Backend MERN Calendar

---
+++ routes/events.js
@@ -0,0 +1,42 @@
+/*
+    Event Routes
+    /api/events
+
+*/
+
+const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+
+const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { isDate } = require('../helpers/isDate');
+
+
+const router = Router();
+
+
+//Todas tienen que pasar por el JWT
+router.use(validarJWT);
+
+//Obtener eventos
+router.get('/', getEventos);
+
+//Crear un nuevo evento
+router.post(
+    '/',
+    [
+        check('title', 'El título es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom( isDate ),
+        check('end', 'La fecha de finalización es obligatoria').custom( isDate ),
+        validarCampos
+    ],
+    crearEvento);
+
+//Actualizar Evento
+router.put('/:id', actualizarEvento);
+
+//Borrar evento
+router.delete('/:id', eliminarEvento);
+
+module.exports = router;
